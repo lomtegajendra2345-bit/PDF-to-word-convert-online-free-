72cf224dea50d0a3aa21e21f8f431e5f280bc1bf
Mr.Gajendra <lomtegajendra2345@gmail.com>
2026-02-25 13:11:32 +0530

    Update convert.js

--- api/convert.js
+++ api/convert.js
@@ -1,5 +1,31 @@
 export default async function handler(req, res) {
 
-res.status(200).json({ message: "API working" });
+const apiKey = process.env.CLOUDCONVERT_API_KEY;
+
+const response = await fetch("https://api.cloudconvert.com/v2/jobs", {
+method: "POST",
+headers: {
+"Authorization": "Bearer " + apiKey,
+"Content-Type": "application/json"
+},
+body: JSON.stringify({
+tasks: {
+"import-my-file": { operation: "import/upload" },
+"convert-my-file": {
+operation: "convert",
+input: "import-my-file",
+output_format: "docx"
+},
+"export-my-file": {
+operation: "export/url",
+input: "convert-my-file"
+}
+}
+})
+});
+
+const data = await response.json();
+
+res.status(200).json(data);
 
 }
